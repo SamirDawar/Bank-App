7fc7d6347686c2a3a015199d78c58a2121808dec
Samir Dawar <samir.dawar01@gmail.com>
2024-08-15 13:40:55 -0400

    Header Box

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,20 @@
+import HeaderBox from '@/components/HeaderBox'
 import React from 'react'
-
 const Home = () => {
+  const loggedIn = {firstName: "Samir"}
   return (
-    <div>
-        Home
-    </div>
+    <section className='home'>
+        <div className='home-content'>
+          <header className='home-header'>
+            <HeaderBox
+            type="greeting"
+            title="welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions effectively"
+            />
+          </header>
+        </div>
+    </section>
   )
 }
 
